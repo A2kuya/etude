75903c3e9aa4767b444d08b596a9bcbac1554fd5
suuK <kswcks1457@gmail.com>
2021-07-07 16:39:52 +0900

    hyena mark

--- etude/Assets/Scripts/Hyena/HyenaBackWalk.cs
+++ etude/Assets/Scripts/Hyena/HyenaBackWalk.cs
@@ -14,6 +14,7 @@ public class HyenaBackWalk : StateMachineBehaviour
     {
         hyena.yFlip(true);
         if(hyena.canDash && hyena.canAttack){
+            hyena.KeepDistance();
             hyena.isClose = false;
             hyena.isMoving = false;
             hyena.yFlip();
