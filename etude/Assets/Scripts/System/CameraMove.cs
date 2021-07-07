390ed75ec1f7e396b4a2f4a10b643e7b94095821
A2kuya <hskim5293@naver.com>
2021-07-07 10:54:19 +0900

    fix cameramoving and platform
    fix cameramoving and platform

--- etude/Assets/Scripts/System/CameraMove.cs
+++ etude/Assets/Scripts/System/CameraMove.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
     public Transform target;        // 따라다닐 타겟 오브젝트의 Transform
-    private Transform tr;                // 카메라 자신의 Transform
+    private Transform camera;                // 카메라 자신의 Transform
 
     Vector3 v;
     // Start is called before the first frame update
     void Start()
     {
-        tr = GetComponent<Transform>();
+       camera = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        v = target.position;
+        if(target.position.y<15f)
+        {
+            if((target.position.x>-3f)&&(target.position.x<136f))
+                v.x = target.position.x + 2f;
+        }
+        else
+        {
+            if ((target.position.x > 54f) && (target.position.x < 111f))
+                v.x = target.position.x + 2f;
+        }
+        v.y = target.position.y+7f;
         v.z = -10f;
-        tr.position = v;
+        camera.position = Vector3.MoveTowards(camera.position, v, 0.3f);
   
     }
 }
